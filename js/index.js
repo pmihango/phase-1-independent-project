72aab1b9f3bbecc0b72e64178463f9caed1ccc5b
pmihango <pmihango@gmail.com>
2023-01-05 11:51:56 +0300

    added all the required URLs in index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// URLS
+//1. Random brewery
+const RANDOM_BREWERY = 'https://api.openbrewerydb.org/breweries/random'
+// 2. List
+const LIST = 'https://api.openbrewerydb.org/breweries'
+// 3. Types
+const BREWERY_TYPE = 'https://api.openbrewerydb.org/breweries?by_type=micro&per_page=3'
+// 4. Search
+const SEARCH = 'https://api.openbrewerydb.org/breweries/search?query={search}'
+
