84ddae779406abecdc3c1f1511306045dbab0008
pmihango <pmihango@gmail.com>
2023-01-11 15:58:59 +0300

    added search in index.js

--- js/script.js
+++ js/script.js
@@ -50,3 +50,30 @@ function displayCocktail(data) {
 
 }
 
+// fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error("NETWORK RESPONSE ERROR");
+//     }
+//   })
+//   .then(data => displayCocktail(data))
+//   .catch((error) => console.error("FETCH ERROR:", error));
+
+// function displayCocktail(data) {
+
+//   const cocktail = data.drinks[0];
+//   const cocktailDiv = document.getElementById("cocktail");    
+
+//   // cocktail name
+//   const cocktailName = cocktail.strDrink;
+//   const heading = document.createElement("h1");
+//   heading.innerHTML = cocktailName;
+//   cocktailDiv.appendChild(heading);
+
+//   // cocktail image
+//   const cocktailImg = document.createElement("img");
+//   cocktailImg.src = cocktail.strDrinkThumb;
+//   cocktailDiv.appendChild(cocktailImg);
+//   // document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
